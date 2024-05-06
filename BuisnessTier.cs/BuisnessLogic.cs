aaf219be5419742d828058c1a6fcb172f2d1e214
Paige Winnett <winnettpaige2@gmail.com>
2024-05-06 16:36:13 -0500

    save

--- BuisnessTier.cs/BuisnessLogic.cs
+++ BuisnessTier.cs/BuisnessLogic.cs
@@ -1,41 +1,39 @@
-namespace FinalProject;
+
+
 using System.Data;
 using Azure.Communication.Email;
+using FinalProject;
+
+bool _continue = true;
+User user;
+GuiTier appGUI = new GuiTier();
+DataTier database = new DataTier();
+
+user = appGUI.Login();
 
 
-class BusinessLogic
+if (database.LoginCheck(user))
 {
-   //Business Logic
-    static async Task Main(string[] args)
+
+    while (_continue)
     {
-        bool _continue = true;
-        User user;
-        GuiTier appGUI = new GuiTier();
-        DataTier database = new DataTier();
+        int option = appGUI.Dashboard(user);
+        string InputEmailContent = null;
+        switch (option)
+        {
 
-        // start GUI
-        user = appGUI.Login();
+            case 1:
+                DataTable tableEmail = database.Email();
+                if (tableEmail != null)
+                    appGUI.DisplayEmail(tableEmail);
 
-       
-        if (database.LoginCheck(user)){
 
-            while(_continue){
-                int option  = appGUI.Dashboard(user);
-                switch(option)
-                {
-                    
-                    case 1:
-                        DataTable tableEmail = database.Email();
-                        if(tableEmail != null)
-                            appGUI.DisplayEmail(tableEmail);
-                             
-                               
-                                string serviceConnectionString = "endpoint=https://finalprojectresource.unitedstates.communication.azure.com/;accesskey=tg1/9vMEtn/qfCQKNjYE8xQ9+aSSMR0VSwlSKBk5JVqd/5sPkJSwhTO1M6e7wfRLOsxnLdB6RyRn2E9gTVkMAQ==";
-                                EmailClient emailclient = new EmailClient(serviceConnectionString);
-                                var subject = "PACKAGE ALERT!";
-                                var emailContent = new EmailContent(subject);
-                                
-                                emailContent.Html= @"
+                string serviceConnectionString = "endpoint=https://finalprojectresource.unitedstates.communication.azure.com/;accesskey=tg1/9vMEtn/qfCQKNjYE8xQ9+aSSMR0VSwlSKBk5JVqd/5sPkJSwhTO1M6e7wfRLOsxnLdB6RyRn2E9gTVkMAQ==";
+                EmailClient emailclient = new EmailClient(serviceConnectionString);
+                var subject = "PACKAGE ALERT!";
+                var emailContent = new Azure.Communication.Email.EmailContent(subject);
+
+                emailContent.Html = @"
                                             <html>
                                                 <body>
                                                     <h1 style=color:red>Legands Apartment Postal Services</h1>
@@ -43,78 +41,85 @@ class BusinessLogic
                                                     <p>We have recieved your package in the office. Due to limited storage, you have 7 days to claim you rpackaged before it is returned to the post office. </p>
                                                 </body>
                                             </html>";
-                           
-                                var sender = "donotreply@5913b1db-61ad-4b4b-8ee3-00c778c69668.azurecomm.net";
 
-                                Console.WriteLine("Please input an email address: ");
-                                string? inputEmail = Console.ReadLine();
-                                var emailClient= new EmailClient(new List<EmailClient> {
-                                    new EmailContent (inputEmailContent) { DisplayName = "Connecting" },
+                var sender = "donotreply@5913b1db-61ad-4b4b-8ee3-00c778c69668.azurecomm.net";
+
+                Console.WriteLine("Please input an email address: ");
+                string? InputEmail = Console.ReadLine();
+                var emailClient = new EmailClient(new List<EmailClient> {
+                                    new Azure.Communication.Email.EmailContent (InputEmailContent) { DisplayName = "Connecting" },
                                 });
 
-                                var emailMessage = new EmailMessage(sender, emailContent, emailClient);
+                var emailMessage = new EmailMessage(sender, emailContent, emailClient);
 
-                                try
-                                {
-                                    SendEmailResult sendEmailResult = emailClient.Send(emailMessage);
+                try
+                {
+                    SendEmailResult sendEmailResult = emailClient.SendAsync(emailMessage);
 
-                                    string messageId = sendEmailResult.MessageId;
-                                    if (!string.IsNullOrEmpty(messageId))
-                                    {
-                                        Console.WriteLine($"Email sent, MessageId = {messageId}");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"Failed to send email.");
-                                        return;
-                                    }
-                            
-                                    var cancellationToken = new CancellationTokenSource(TimeSpan.FromMinutes(2));
-                                    do
-                                    {
-                                        SendStatusResult sendStatus = emailResident.GetSendStatus(messageId);
-                                        Console.WriteLine($"Send mail status for MessageId : <{messageId}>, Status: [{sendStatus.Status}]");
+                    string emailId = sendEmailResult.emailId;
+                    if (!string.IsNullOrEmpty(emailId))
+                    {
+                        Console.WriteLine($"Email sent, EmailId = {emailId}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to send email.");
+                        return;
+                    }
 
-                                        if (sendStatus.Status != SendStatus.Queued)
-                                        {
-                                            break;
-                                        }
-                                        await Task.Delay(TimeSpan.FromSeconds(10));
-                                    
-                                    } while (!cancellationToken.IsCancellationRequested);
+                    var cancellationToken = new CancellationTokenSource(TimeSpan.FromMinutes(2));
+                    do
+                    {
+                        SendStatusResult sendStatus = emailContent.GetSendStatus(emailId);
+                        Console.WriteLine($"Send mail status for MessageId : <{emailId}>, Status: [{sendStatus.Status}]");
 
-                                    if (cancellationToken.IsCancellationRequested)
-                                    {
-                                        Console.WriteLine($"Email send Failure");
-                                    }
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine($"Error in sending email, {ex}");
-                                }
-                        break;
+                        if (sendStatus.Status != sendStatus.Queued)
+                        {
+                            break;
+                        }
+                        await Task.Delay(TimeSpan.FromSeconds(10));
 
-                    
-                    case 2:
-                    DataTable TablePackageHistory = database.ShowRecords();
-                        if(TablePackageHistory != null)
-                            appGUI.DisplayPackageHistory(TablePackageHistory);
-                        break;
-                    
-                    case 3:
-                        _continue = false;
-                        Console.WriteLine("Log out, Goodbye.");
-                        break;
-                   
-                    default:
-                        Console.WriteLine("Wrong Input");
-                        break;
+                    } while (!cancellationToken.IsCancellationRequested);
+
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        Console.WriteLine($"Email send Failure");
+                    }
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in sending email, {ex}");
+                }
+                break;
 
-            }
+
+            case 2:
+                DataTable TablePackageHistory = database.ShowRecords();
+                if (TablePackageHistory != null)
+                    appGUI.DisplayPackageHistory(TablePackageHistory);
+                break;
+
+            case 3:
+                _continue = false;
+                Console.WriteLine("Log out, Goodbye.");
+                break;
+
+            default:
+                Console.WriteLine("Wrong Input");
+                break;
         }
-        else{
-                Console.WriteLine("Login Failed, Try Again.");
-        }        
-    }    
+
+    }
+}
+else
+{
+    Console.WriteLine("Login Failed, Try Again.");
+}
+
+internal class SendStatusResult
+{
+}
+
+internal class SendEmailResult
+{
 }
--- BuisnessTier.cs/StaffLogin.cs
+++ BuisnessTier.cs/StaffLogin.cs
@@ -1,6 +1,27 @@
+using Google.Protobuf.WellKnownTypes;
+
 namespace FinalProject;
 class User{
-    // public int UserId {get;set;}
-    public string userID {get;set;} =string.Empty;
-    public string userPassword {get;set;} = string.Empty;
+
+    public string UserID {get;set;} =string.Empty;
+    public string UserPassword {get;set;} = string.Empty;
+}
+
+class EmailContent{
+    private string subject;
+
+    public EmailContent(string subject)
+    {
+        this.subject = subject;
+    }
+
+    public string MessageId {get;set;} = string.Empty;  
+    public string EmailClient {get;set;} = string.Empty;
+    public string DisplayName {get;set;} = string.Empty; 
+    public string EmailMessage {get;set;} = string.Empty; 
+    public string InputEmailContent {get;set;} = string.Empty;
+    public string SendEmailResult {get;set;} = string.Empty;
+    public string Html { get; set; } = string.Empty;
+    public string GetSendStatus {get;set;} = string.Empty;
+    public string SendStatus {get;set;} = string.Empty;
 }
